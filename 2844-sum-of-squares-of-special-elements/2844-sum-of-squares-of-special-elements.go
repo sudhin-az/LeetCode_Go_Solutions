3f38a49c07feef8747e455afe466b37b3423cdbf
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2024-12-12 17:02:27 +0530

    Time: 1 ms (19.44%), Space: 5.1 MB (6.82%) - LeetHub

--- 2844-sum-of-squares-of-special-elements/2844-sum-of-squares-of-special-elements.go
+++ 2844-sum-of-squares-of-special-elements/2844-sum-of-squares-of-special-elements.go
@@ -1,7 +1,7 @@
 func sumOfSquares(nums []int) (c int) {
-   for i, v := range nums {
+   for i, j := range nums {
     if len(nums) % (i+1) == 0 {
-        c += v * v
+        c += j * j
     }
    }
    return
