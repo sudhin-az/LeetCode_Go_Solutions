f228f9c39f04b7bdae89c1fdfb04f908c5e97ffc
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-04-19 03:05:20 +0530

    Time: 22 ms (23.1%), Space: 5.4 MB (75.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,3 +1,12 @@
 func twoSum(nums []int, target int) []int {
-   
+    result := []int{}
+    for i:=0; i<len(nums); i++ {
+        for j:= i+1; j<len(nums); j++ {
+            if nums[i] + nums[j] == target {
+                result = append(result, i)
+                result = append(result, j)
+            }
+        }
+    }
+    return result
 }
