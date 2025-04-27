f21dc7cdd8f66b550a441a73b7fa189a4bae3c02
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-04-27 21:10:54 +0530

    Time: 5 ms (67.09%), Space: 5.8 MB (51.24%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.go
@@ -0,0 +1,24 @@
+func numIslands(grid [][]byte) int {
+    count := 0
+    for i:=0; i<len(grid); i++ {
+        for j:=0; j<len(grid[i]); j++ {
+            if grid[i][j] == '1' {
+                dfs(grid, i, j)
+                count ++
+            }
+        }
+    }
+    return count
+}
+
+func dfs(grid [][]byte, i, j int)  {
+    if i < 0 || i>=len(grid) || j<0 || j>=len(grid[i]) || grid[i][j] != '1' {
+        return
+    }
+    grid[i][j] = '0'
+
+    dfs(grid, i+1, j)
+    dfs(grid, i-1, j)
+    dfs(grid, i, j+1)
+    dfs(grid, i, j-1)
+}
