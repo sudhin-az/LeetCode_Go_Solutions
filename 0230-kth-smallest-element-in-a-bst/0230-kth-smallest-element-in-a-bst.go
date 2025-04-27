14ac11a5303404e118971fd642488dd4eb8b3773
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-04-27 20:13:06 +0530

    Time: 0 ms (100%), Space: 8.5 MB (18.05%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+    stack := []*TreeNode{}
+    curr := root
+    for root != nil {
+        for curr != nil {
+            stack = append(stack, curr)
+            curr = curr.Left
+        }
+        curr = stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        k--
+
+        if k == 0 {
+            return curr.Val
+        }
+        curr = curr.Right
+    }
+    return -1
+}
