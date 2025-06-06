8071c7a739f6532fb883003dc52d42dc6fc20577
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-06-06 09:50:05 +0530

    Time: 0 ms (100%), Space: 4.1 MB (71.15%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.go
@@ -0,0 +1,10 @@
+func checkIfPangram(sentence string) bool {
+    store := make(map[rune]struct{})
+
+    for _, ch := range sentence {
+        if _, ok := store[ch]; !ok {
+            store[ch] = struct{}{}
+        }
+    }
+    return len(store) == 26
+}
