b9c79da74c62b7e925e41a63f630d3eeaf6a2650
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-08-20 08:48:39 +0530

    Time: 0 ms (100%), Space: 5.1 MB (53.75%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.go
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.go
@@ -1,13 +1,11 @@
 func finalValueAfterOperations(operations []string) int {
     x := 0
     for i:=0; i<len(operations); i++ {
-        fmt.Println("sudhin",operations[i] == "++x" || operations[i] =="x++")
-        fmt.Println(x + 1)
-        if operations[i] == "++X" || operations[i] =="X++" {
+        if operations[i] == "++X" || operations[i] == "X++" {
             x++
         } else if operations[i] == "--X" || operations[i] == "X--" {
             x--
         }
-        }     
+    }
     return x
 }
