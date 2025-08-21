3554e0a4e19dbf03ba9a6914383b2d0f2e89c076
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-08-21 10:48:11 +0530

    Time: 0 ms (100%), Space: 4.2 MB (81.94%) - LeetHub

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/3476-find-minimum-operations-to-make-all-elements-divisible-by-three.go
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/3476-find-minimum-operations-to-make-all-elements-divisible-by-three.go
@@ -1,9 +1,9 @@
 func minimumOperations(nums []int) int {
-   count := 0
-   for _, v := range nums {
-    if v % 3 != 0 ||3+ v % 3 == 0 || 3 - v % 3 == 0 {
-        count ++
+    count := 0
+    for _, v := range nums {
+        if v % 3 != 0 || 3 + v % 3 == 0 || 3 - v % 3 == 0 {
+            count ++
+        }
     }
-   }
-   return count
+    return count
 }
