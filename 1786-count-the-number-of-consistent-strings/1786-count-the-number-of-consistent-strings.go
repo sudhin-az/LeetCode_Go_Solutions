31e90577dd8df158b402e2827283bd8d4716d6f8
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-09-07 13:43:26 +0530

    Time: 30 ms (64.84%), Space: 8.8 MB (66.41%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.go
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.go
@@ -1,13 +1,14 @@
 func countConsistentStrings(allowed string, words []string) int {
     count := 0
-   for _, s := range words {
-   for k, v := range s {
-    if strings.Contains(allowed, string(v)) == false {
-        break
+    for _, s := range words {
+        for k, v := range s {
+            if strings.Contains(allowed, string(v)) == false {
+                break
+            }
+            if k == len(s) - 1 {
+                count ++ 
+            }
+        }
     }
-    if k == len(s) - 1 {
-        count ++ 
-    }}
-   }
-   return count
+    return count 
 }
