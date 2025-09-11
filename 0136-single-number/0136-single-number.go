a3d4bd45e01fe5728300af4a39d01216d161b57c
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-09-11 17:40:04 +0530

    Time: 356 ms (5.72%), Space: 7.7 MB (94.72%) - LeetHub

---
+++ 0136-single-number/0136-single-number.go
@@ -0,0 +1,14 @@
+func singleNumber(nums []int) int {
+    for i:=0; i<len(nums); i++ {
+        count := 0
+        for j:=0; j<len(nums); j++ {
+            if nums[i] == nums[j] {
+               count ++
+            }
+        }
+        if count == 1 {
+            return nums[i]
+        }
+    }
+    return 0
+}
