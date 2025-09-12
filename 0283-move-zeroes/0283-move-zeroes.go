f98b378f537011fde0792e89d4de7ebab3de6c1b
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-09-12 09:10:00 +0530

    Time: 6 ms (14.88%), Space: 9.8 MB (6.47%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -0,0 +1,9 @@
+func moveZeroes(nums []int)  {
+    count := 0
+    for i:=0; i<len(nums); i++ {
+        if nums[i] != 0 {
+            nums[i], nums[count] = nums[count], nums[i]
+            count ++
+        }
+    }
+}
