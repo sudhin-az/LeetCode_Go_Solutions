8f092e989c94be6441bab8d2f1503fbfc21daee1
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-09-19 08:12:19 +0530

    Time: 0 ms (100%), Space: 8 MB (62.07%) - LeetHub

--- 2454-largest-local-values-in-a-matrix/2454-largest-local-values-in-a-matrix.go
+++ 2454-largest-local-values-in-a-matrix/2454-largest-local-values-in-a-matrix.go
@@ -1,8 +1,8 @@
 func largestLocal(grid [][]int) [][]int {
     n := len(grid)
     res := make([][]int, n-2)
-    for i:= range res {
-        res[i] = make([]int, n - 2)
+    for i := range res {
+        res[i] = make([]int, n-2)
     }
 
     for i:=0; i<n-2; i++ {
