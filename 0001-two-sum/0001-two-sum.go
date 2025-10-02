39b257b986f8bcf3609ef0b72a22fbaf4038d92c
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-10-02 16:45:35 +0530

    Time: 0 ms (100%), Space: 6 MB (40.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -2,6 +2,7 @@ func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
     for i, num := range nums {
         diff := target - num
+        target = diff + num
         if j, found := m[diff]; found {
             return []int{i, j}
         }
