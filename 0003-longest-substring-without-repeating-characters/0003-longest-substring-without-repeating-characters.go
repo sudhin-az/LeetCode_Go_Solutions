96244e1f47f3abf17eaaac9fbc7a8c2ed0d4bd44
Sudhin Az <163923216+sudhin-az@users.noreply.github.com>
2025-10-02 12:30:36 +0530

    Time: 4 ms (69.5%), Space: 4.5 MB (86.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
@@ -2,13 +2,11 @@ func lengthOfLongestSubstring(s string) int {
     chars := make(map[rune]bool)
     left := 0
     maxLen := 0
-
-    for right := 0; right < len(s); right ++ {
+    for right:=0; right<len(s); right++ {
         ch := rune(s[right])
-
         for chars[ch] {
             delete(chars, rune(s[left]))
-            left ++
+            left++
         }
         chars[ch] = true
         currentLen := right - left + 1
